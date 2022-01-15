6ebd837d7e1089c605a8f010db0ea8dbb6975d13
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 15:42:39 +0900

    feat: swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './middlewares/users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
-
+import { UsersService } from './middlewares/users/users.service';
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
-// export class AppModule {}
 
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`listening on port ${port}`)
+
+  const config = new DocumentBuilder()
+    .setTitle('TEST API')
+    .setDescription('TEST 개발을 위한 API 문서입니다.')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  
+  await app.listen(port);
+  console.log(`listening on port ${port}`)
 }
 bootstrap();
