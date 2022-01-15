01817e2631d07945b66b8a312fcdc173eca5eda9
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 12:03:06 +0900

    feat: set query, param, body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
-
-@Controller('users')
+import { Controller, Get, Post, Req, Res, Body } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
+@Controller('api/users')
 export class UsersController {
+  constructor(private userService: UsersService) {
+  }
+  
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
   
   @Post()
-  postUsers() {}
+  postUsers(@Body() data: JoinRequestDto) {
+    this.userService.postUsers(data.email, data.nickname, data.password)
+  }
   
   @Post('login')
   logIn() {
