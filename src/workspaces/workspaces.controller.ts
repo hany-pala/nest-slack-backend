9151802bbb9c1818003428e0b82ea64c9672f612
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 14:57:00 +0900

    Create workspaces.controller.ts

---
+++ src/workspaces/workspaces.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Delete } from '@nestjs/common';
+
+@Controller('api/workspaces')
+export class WorkspacesController {
+  @Get()
+  getMyWorkspaces() {
+    
+  }
+  
+  @Post()
+  createWorkspace() {
+    
+  }
+  
+  @Get(':url/members')
+  getAllMembersFromWorkspace() {
+    
+  }
+  
+  @Post(':url/members/:id')
+  inviteMembersToWorksapce() {
+    
+  }
+  
+  @Delete(':url/members/:id')
+  kickMemberFromWorkspace() {
+  
+  }
+  
+  @Get(':url/members/:id')
+  getMemberInfoInWorkspace() {
+  }
+  
+  
+}
