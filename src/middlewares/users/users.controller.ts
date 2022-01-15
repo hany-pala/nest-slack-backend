0c38426306aa36197c52e178ca12d73d1318867d
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 16:04:14 +0900

    feat undefined to null interceptor

--- src/middlewares/users/users.controller.ts
+++ src/middlewares/users/users.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Post, Req, Res, Body } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Controller, Get, Post, Req, Res, Body, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
+import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptors';
 import { UserDto } from 'src/common/user.dto';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+
+@UseInterceptors(UndefinedToNullInterceptor)
+@ApiTags("USER")
 @Controller('api/users')
 export class UsersController {
   constructor(private userService: UsersService) {
   }
   
-  @ApiOperation({ summary: '내 정보 조회' })
   @ApiResponse({
     type: UserDto,
     status:200,
@@ -18,23 +22,30 @@ export class UsersController {
     status:500,
     description: '서버 에러'
   })
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
-  getUsers(@Req() req) {
-    return req.user;
+  getUsers(@User() user) {
+    return user;
   }
+  
+  
   @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.userService.postUsers(data.email, data.nickname, data.password)
   }
+  
+  
   @ApiResponse({
     type: UserDto,
     status:200,
     description: '성공'
   })
   @Post('login')
-  logIn() {
+  logIn(@User() user) {
+    return user;
   }
+  
   
   @Post('logout')
   logOut(@Req() req, @Res() res) {
