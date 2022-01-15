866ab2c5112d3bf6379b05e448c287f788aea4d7
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 15:42:56 +0900

    feat user-dto

---
+++ src/common/user.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { JoinRequestDto } from 'src/middlewares/users/dto/join.request.dto';
+
+export class UserDto extends JoinRequestDto {
+  @ApiProperty({
+    required: true,
+    example: 1,
+    description: '아이디'
+  })
+  @ApiProperty({
+    required: true,
+    example: 'inspiration33@kakao.com',
+    description: '이메일'
+  })
+  
+  id: number;
+  email: string;
+}
