5f1b902553005fb922642a55bf07f94ddf80ea8c
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 15:43:23 +0900

    feat: join-requset-dto

---
+++ src/middlewares/users/dto/join.request.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+/**
+ * nest convention
+ * export default 대신에 export 사용하기
+ * interface 보다는 class로 사용
+ * code convention은 - 보다는 . 사용
+ */
+export class JoinRequestDto  {
+  @ApiProperty({
+    example: 'inspiration33@kakao.com',
+    description: '이메일',
+    required: true,
+  })
+  public email: string;
+  
+  @ApiProperty({
+    example: 'inspiration33',
+    description: '아이디',
+    required: true,
+  })
+  public nickname: string;
+  
+  @ApiProperty({
+    example: 'nodejsbook',
+    description: '비밀번호',
+    required: true,
+  })
+  public password: string;
+}
