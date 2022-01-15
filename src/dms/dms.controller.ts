855ba5a47a32c8c209c3a20612d225a2f86369b9
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-15 14:56:56 +0900

    feat: co, s, mo in workspaces

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -6,8 +6,6 @@ export class DmsController {
   getChat(@Query('perPage') perPage, @Query('page') page, @Param() param) {
     console.log(param.id, param.url);
     console.log(perPage, page);
-    // getChat(@Query() query) {
-    // console.log(query.perPage, query.page)
   }
   
   @Post(':id/chats')
